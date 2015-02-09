9433764fc875b9dd3fcd66d0db41eaac2d47a1b4
damjosu <damjosu@gmail.com>
2015-02-09 09:35:29 +0100

    Metodos printProductDetails, finProduct, y numberInStock modificados para que funcionen.

---
+++ StockManager.java
@@ -0,0 +1,94 @@
+import java.util.ArrayList;
+
+/**
+ * Manage the stock in a business.
+ * The stock is described by zero or more Products.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StockManager
+{
+    // A list of the products.
+    private ArrayList<Product> stock;
+
+    /**
+     * Initialise the stock manager.
+     */
+    public StockManager()
+    {
+        stock = new ArrayList<Product>();
+    }
+
+    /**
+     * Add a product to the list.
+     * @param item The item to be added.
+     */
+    public void addProduct(Product item)
+    {
+        stock.add(item);
+    }
+    
+    /**
+     * Receive a delivery of a particular product.
+     * Increase the quantity of the product by the given amount.
+     * @param id The ID of the product.
+     * @param amount The amount to increase the quantity by.
+     */
+    public void delivery(int id, int amount)
+    {
+    }
+    
+    /**
+     * Try to find a product in the stock with the given id.
+     * @return The identified product, or null if there is none
+     *         with a matching ID.
+     */
+    public Product findProduct(int id)
+    {
+        int index = 0;      
+        Product productMatch = null;
+        boolean productFinded = false;
+        while (!productFinded)        
+        {            
+            if (stock.get(index).getID() == id) 
+            {
+                productMatch = stock.get(index);
+                productFinded = true;
+            }
+            index++;
+        }
+        return productMatch;
+    }
+    
+    /**
+     * Locate a product with the given ID, and return how
+     * many of this item are in stock. If the ID does not
+     * match any product, return zero.
+     * @param id The ID of the product.
+     * @return The quantity of the given product in stock.
+     */
+    public int numberInStock(int id)
+    {
+        int i = 0;
+        for (Product product : stock)
+        {
+            if (product.getID() == id)
+            {
+                i++;
+            }            
+        }
+        return i;
+    }
+        
+    /**
+     * Print details of all the products.
+     */
+    public void printProductDetails()
+    {
+        for (Product product : stock)
+        {
+            System.out.println(product.toString());
+        }
+    }
+}
